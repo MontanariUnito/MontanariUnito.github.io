4abefd6afbf31846bea26a89422c61fdd11b578b
Malasaur <malasaur@tuta.io>
2026-03-05 23:21:44 +0100

    Updated Player component

--- site/.vitepress/theme/components/Player.vue
+++ site/.vitepress/theme/components/Player.vue
@@ -1,7 +1,19 @@
 <script setup>
-import { shallowRef, onMounted } from "vue";
+import { computed, shallowRef, onMounted, useAttrs } from "vue";
 
 const Player = shallowRef(null);
+const attrs = useAttrs();
+const playerAttrs = computed(() => {
+    const { style, ...rest } = attrs;
+
+    return {
+        backgroundColor: "var(--vp-c-bg)",
+        primaryColor: "var(--vp-c-brand-1)",
+        showPlaybackRate: true,
+        ...rest,
+        style: [{ border: "1px solid var(--vp-c-divider)" }, style],
+    };
+});
 
 onMounted(async () => {
     const mod = await import("vue-wave-player");
@@ -11,5 +23,5 @@ onMounted(async () => {
 </script>
 
 <template>
-    <component v-if="Player" :is="Player" v-bind="$attrs" />
+    <component v-if="Player" :is="Player" v-bind="playerAttrs" />
 </template>
